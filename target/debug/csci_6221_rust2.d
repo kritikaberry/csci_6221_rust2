e76c88f4623a2858b2f3f362b9e5a77b17280421
Kritika Berry <kritikaberry@gmail.com>
2025-11-06 18:58:54 -0500

    Initial Rust Project Setup

---
+++ target/debug/csci_6221_rust2.d
@@ -0,0 +1,1 @@
+/Users/kritikaberry/Desktop/GWU/GWU\ SEM\ 1/csci_6221_rust2/target/debug/csci_6221_rust2: /Users/kritikaberry/Desktop/GWU/GWU\ SEM\ 1/csci_6221_rust2/src/main.rs
---
+++ target/debug/deps/csci_6221_rust2-3c23b14b6f90c3b8.d
@@ -0,0 +1,5 @@
+/Users/kritikaberry/Desktop/GWU/GWU SEM 1/csci_6221_rust2/target/debug/deps/csci_6221_rust2-3c23b14b6f90c3b8.d: src/main.rs
+
+/Users/kritikaberry/Desktop/GWU/GWU SEM 1/csci_6221_rust2/target/debug/deps/csci_6221_rust2-3c23b14b6f90c3b8: src/main.rs
+
+src/main.rs:
